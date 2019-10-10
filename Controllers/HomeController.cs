6f770fdcde04ef1fee8b6f21369a345015d0f6a2
weekswm <weekswm@gmail.com>
2019-10-10 15:18:51 -0700

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DodgersFanSite.Models;
+
+namespace DodgersFanSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ViewResult Book()
+        {
+            return View();
+        }
+
+        public ViewResult History()
+        {
+            return View();
+        }
+        public ViewResult Links()
+        {
+            return View();
+        }
+        public ViewResult Sources()
+        {
+            return View();
+        }
+        public ViewResult Stories()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
